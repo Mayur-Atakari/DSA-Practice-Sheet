437e41ebf3410b2829593b5ec8f412f82e312637
Mayur Atakari <atakarimayur@gmail.com>
2026-02-03 16:20:08 +0530

    poblems of function

---
+++ Funtions/HelloFunction.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void hello(int n){
+    for(int i = 1; i<=n; i++){
+        cout << "I am learning functions" << endl;
+    }
+}
+
+int main() {
+	// your code goes here
+	int n;
+	cin >> n;
+	hello(n);
+    return 0;
+}
